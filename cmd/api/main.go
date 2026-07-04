e20dd24ef5453bed43d5cc47d294dd6decac8d14
Dhwanilv26 <dhwanil2604@gmail.com>
2026-07-04 20:20:28 +0530

    setup air.toml file and installed psql locally

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("server created")
+
+	var router *gin.Engine = gin.Default()
+	router.SetTrustedProxies(nil)
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "todo api is running",
+			"status":  "success",
+		})
+	})
+
+	router.Run(":3000")
 }
