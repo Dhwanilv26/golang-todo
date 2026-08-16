b179c36411b799f368d45fad7db7dd6c79679806
Dhwanilv26 <dhwanil2604@gmail.com>
2026-08-16 16:16:00 +0530

    created user repository

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,70 @@
+package repository
+
+import (
+	"todo-api/internal/models"
+
+	"context"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func CreateUser(pool *pgxpool.Pool, user *models.User) (*models.User, error) {
+
+	var ctx context.Context
+	var cancel context.CancelFunc
+
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var query string = `
+		INSERT INTO users (email, password)
+		VALUES ($1, $2)
+		RETURNING id, email, created_at, updated_at
+	`
+
+	var err error = pool.QueryRow(ctx, query, user.Email, user.Password).Scan(
+		&user.ID,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+
+}
+
+func GetUserByEmail(pool *pgxpool.Pool, email string) (*models.User, error) {
+
+	var ctx context.Context
+	var cancel context.CancelFunc
+
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var query string = `
+		SELECT id ,email ,password ,created_at ,updated_at
+		FROM users
+		WHERE email = $1
+	`
+	var user models.User
+	var err = pool.QueryRow(ctx, query, email).Scan(
+		&user.ID,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+
+}
